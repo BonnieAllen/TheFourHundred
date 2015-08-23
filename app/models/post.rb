b7ad2f43f328c6c2dc5e81380a6fbdc51fa0b1c8
Bonnie <aalle.allen@gmail.com>
2015-08-23 16:44:57 -0400

    implemented post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ActiveRecord::Base
+  has_attached_file :image, styles: { :medium => "640x" }
+  has_many :comments, dependent: :destroy  
+  has_many :uploads
+  validates :image, presence: true
+  validates :user_id, presence: true
+
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  belongs_to :user 
 end
